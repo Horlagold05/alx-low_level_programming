526c904693e93a42d54f581a0a9d7a99e4794a07
jimoh abdulrasheed Olaitan <jimohabdulrasheed934@gmail.com>
2022-12-09 22:10:07 -0800

    First C project

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,14 +2,20 @@
 /**
  * main - Entry point
  *
- * Return: 0
-*/
+ * Return: Always 0
+ */
 int main(void)
 {
-	printf("size of a char: %lu byte(S)\n", (unsigned long)sizeof(char));
-	printf("size of an int: %lu byte(S)\n", (unsigned long)sizeof(int));
-	printf("size of a long int: %lu byte(S)\n", (unsigned long)sizeof(long int));
-	printf("size of a long long int: %lu byte(S)\n", (unsigned long)sizeof(long long int);
-	printf("size of a float: %lu byte(S)\n", (unsigned long)sizeof(float));
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+	printf("Size of a char: %lu byte(S)\n", (unsigned long)sizeof(a));
+	printf("Size of an int: %lu byte(S)\n", (unsigned long)sizeof(b));
+	printf("Size of a long int: %lu byte(S)\n", (unsigned long)sizeof(c));
+	printf("Size of a long long int: %lu byte(S)\n", (unsigned long)sizeof(d));
+	printf("Size of a float: %lu byte(S)\n", (unsigned long)sizeof(f));
 	return (0);
 }
