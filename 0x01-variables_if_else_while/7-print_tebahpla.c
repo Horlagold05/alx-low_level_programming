9e0772d84096417be134db87f029f098e1820fcc
jimoh abdulrasheed Olaitan <jimohabdulrasheed934@gmail.com>
2022-12-11 22:45:20 -0800

    First C project

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - Prints the lowercase alphabet in reverse
+ * main - Prints the lowercase alphabet in reverse.
  *
- * Return: 0
+ * Return: 0.
  */
 int main(void)
 {
